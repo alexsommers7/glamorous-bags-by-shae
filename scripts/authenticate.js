61f98d27715a7b3ad5c9e08f552c090e98d11179
alexsommers7 <asommers22@hotmail.com>
2021-02-06 17:58:38 -0700

    adding files

---
+++ scripts/authenticate.js
@@ -0,0 +1,26 @@
+// Get Cookie Function
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// Check Authentication Function
+function checkAuthentication() {
+  var authentication = getCookie("authenticated");
+  if (authentication != "true" && window.location.href != "https://glamorousbagsbyshae.com/gateway") window.location.replace("/gateway");
+  else if (authentication != "true") return "not authenticated";
+  else return "authenticated";
+}
+
+checkAuthentication();
