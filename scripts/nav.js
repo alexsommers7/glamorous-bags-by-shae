46fb162e88cef3f6bdd4491bf38f1cdd7035f16a
Alex Sommers <58455820+alexsommers7@users.noreply.github.com>
2021-03-19 21:53:31 -0700

    clean up nav script

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,20 +5,14 @@ const navParent = document.querySelector(".nav");
 const navToggle = document.querySelector("#navi-toggle");
 
 // toggle hamburger to x and open bg/nav list on nav button or item click
-navParent.addEventListener("click", openNav);
-
-// avoiding arrow syntax for ie 11 support
-function openNav() {
-    navToggle.classList.toggle("nav-open");
-};
-
-
+navParent.addEventListener("click", function() {
+  navToggle.classList.toggle("nav-open");
+});
 
 // SMOOTH SCROLL FOR ANCHOR LINKS
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
