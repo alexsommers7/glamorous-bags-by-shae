d0da0a52851bd03949c8b8f7c8ec95bbd849c068
alexsommers7 <asommers22@hotmail.com>
2021-04-10 19:01:58 -0700

    updating password

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -2,10 +2,10 @@
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
+  var ca = decodedCookie.split(";");
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -18,7 +18,8 @@ function getCookie(cname) {
 // Function to Check Authentication
 function checkAuthentication() {
   var authentication = getCookie("authenticated");
-  if (authentication != "true" && window.location.href != "https://glamorousbagsbyshae.com/gateway") window.location.replace("/gateway");
+  if (authentication != "true" && window.location.href != "https://glamorousbagsbyshae.com/gateway")
+    window.location.replace("/gateway");
   else if (authentication != "true") return "not authenticated";
   else return "authenticated";
 }
