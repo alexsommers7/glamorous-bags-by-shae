5a45104612ae883ec29e6febe6958640b1cf6374
alexsommers7 <asommers22@hotmail.com>
2021-06-26 14:37:05 -0700

    code cleanup and accessibility improvements

--- scripts/totop.js
+++ scripts/totop.js
@@ -2,13 +2,12 @@
 const toTopBtn = document.querySelector(".toTop");
 
 window.onscroll = () => {
-    (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
-    ? toTopBtn.style.display = "flex"
-    : toTopBtn.style.display = "none";
+  document.body.scrollTop > 500 || document.documentElement.scrollTop > 500
+    ? (toTopBtn.style.display = "flex")
+    : (toTopBtn.style.display = "none");
 };
 
 // scroll to top on click
-toTopBtn.addEventListener("click", function() {
-    document.body.scrollTop = 0; // safari
-    document.documentElement.scrollTop = 0; // chrome, firefox, and Opera
+toTopBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
